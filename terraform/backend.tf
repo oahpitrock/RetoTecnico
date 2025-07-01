032ffe216f8a43b42886014b321092a1aba63b5f
oahpitrock <osmarhidalgo8@gmail.com>
2025-07-01 11:39:55 -0300

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket         = "mi-terraform-state-bucket"   
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
